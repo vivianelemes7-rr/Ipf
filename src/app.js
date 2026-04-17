f3a07728d804acca7493329e8560534fff8d5015
vivianelemes7-rr <vivianelemes7@gmail.com>
2026-04-17 03:36:15 -0300

    feat: setup inicial e estrutura de pastas concluída

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+module.exports = app;
